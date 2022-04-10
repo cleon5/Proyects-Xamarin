d879347e9405592306fdf65b37968fa2339865ba
cleon5 <cleon5@ucol.mx>
2022-04-10 08:47:39 -0500

    Gestos prueba

---
+++ gesture/gesture/gesture/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gesture
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSwiped(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+        async void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+
+            await Navigation.PushAsync(new Page1());
+        }
+        async void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
---
+++ gesture/gesture/gesture/Page1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace gesture
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        async void OnSwiped(object sender, EventArgs args)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
